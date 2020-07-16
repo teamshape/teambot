8640a1d0cdcedda1347faa684abefc0f61504f6d
Team Shape <66860026+teamshape@users.noreply.github.com>
2020-07-16 11:10:08 +1000

    Lints commands.

--- commands/pull.js
+++ commands/pull.js
@@ -19,7 +19,7 @@ module.exports = {
 		const guild = message.guild.id;
 
 		let loadedCommandUser = [];
-		
+
 		try {
 			loadedCommandUser = await db.UserDB.findOne({ where: {
 				guild: guild,
@@ -32,17 +32,19 @@ module.exports = {
 
 		if (loadedCommandUser.dataValues.permission >= ADMINISTRATOR) {
 			require('simple-git')()
-			.exec(() => console.log('Starting pull...'))
-			.pull('origin', 'master', {'--rebase': 'true'}, (err, update) => {
-				if (update && update.summary.changes) {
-					console.log(update);
-					require('child_process').exec('npm install').exec('pm2 restart TeamBot');
-					message.channel.send(`TeamBot has been updated with ${update.summary.changes} change(s), ${update.summary.insertions} insertion(s) and ${update.summary.deletions} deletion(s).`);
-				} else {
-					message.channel.send(`No updates available.`);
-				}
-			})
-			.exec(() => console.log('pull done.'));
-		};
+				.exec(() => console.log('Starting pull...'))
+				.pull('origin', 'master', { '--rebase': 'true' }, (err, update) => {
+					if (update && update.summary.changes) {
+						console.log(update);
+						require('child_process').exec('npm install');
+						require('child_process').exec('pm2 restart TeamBot');
+						message.channel.send(`TeamBot has been updated with ${update.summary.changes} change(s), ${update.summary.insertions} insertion(s) and ${update.summary.deletions} deletion(s).`);
+					}
+					else {
+						message.channel.send('No updates available.');
+					}
+				})
+				.exec(() => console.log('pull done.'));
+		}
 	},
 };
