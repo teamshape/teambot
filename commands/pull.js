b740ea08c038917fea8ac8b13a8461d35fa1fd6d
Team Shape <66860026+teamshape@users.noreply.github.com>
2020-07-17 09:32:39 +1000

    Adds numbers to the pull command.

--- commands/pull.js
+++ commands/pull.js
@@ -29,7 +29,7 @@ module.exports = {
 						console.log(update);
 						require('child_process').exec('npm install');
 						require('child_process').exec('pm2 restart TeamBot');
-						message.channel.send(`TeamBot has been updated with ${pluralize('change', update.summary.changes)} change(s), ${ pluralize('update', update.summary.insertions)} insertion(s) and ${ pluralize('deletion', update.summary.deletions)} deletion(s).`);
+						message.channel.send(`TeamBot has been updated with ${update.summary.changes} ${pluralize('change', update.summary.changes)} change(s), ${update.summary.insertions} ${ pluralize('update', update.summary.insertions)} insertion(s) and ${update.summary.deletions} ${ pluralize('deletion', update.summary.deletions)} deletion(s).`);
 					}
 					else {
 						message.channel.send('No updates available.');
