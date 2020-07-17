22e9640f0bbeb490d4e65b669b31fccc16e38314
Team Shape <66860026+teamshape@users.noreply.github.com>
2020-07-17 19:13:55 +1000

    Adds name and activity.

--- teambot.js
+++ teambot.js
@@ -34,6 +34,14 @@ bot.once('ready', () => {
 	console.log(`${bot.user.tag} v1.0.0 loaded in ${timerEnd} seconds!`);
 	saySomething(`${bot.user.tag} v1.0.0 loaded in ${timerEnd} seconds!`);
 
+	bot.user.setUsername('TeamBot Nguyen')
+	.then(user => console.log(`Username set to ${user.username}`))
+	.catch(console.error);
+
+	bot.user.setPresence({ activity: { name: '光復香港，時代革命', type: 'WATCHING' }, status: 'online' })
+	.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+	.catch(console.error);
+
 	const job = new CronJob('0 * * * * *', async function() {
 		const reminders = await teambot.db.RemindDB.findAll({ where: {
 			reminder_timestamp: {
