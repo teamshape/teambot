9181486aed58ada7e2de8c44674e7e02839b992b
Team Shape <66860026+teamshape@users.noreply.github.com>
2020-07-17 14:33:02 +1000

    Adds deployment info.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
 	apps : [{
 		name: 'TeamBot',
 		script: 'teambot.js',
+		exp_backoff_restart_delay: 100,
+		watch: false,
 		env: {
 			NODE_ENV: 'development',
-			watch: '.',
 		},
 		env_production: {
 			NODE_ENV: 'production',
