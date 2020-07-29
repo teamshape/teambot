7eab1572aa5cdf22c30d1af84881b1c2656bd3df
Team Shape <66860026+teamshape@users.noreply.github.com>
2020-07-29 17:26:18 +1000

    Adds report functionality.

---
+++ commands/report.js
@@ -0,0 +1,28 @@
+const { ADMINISTRATOR, OPERATOR, PREMIUM, STANDARD } = require('../permissions');
+
+module.exports = {
+	name: 'report',
+	description: 'Reports users for rule breaking',
+	args: true,
+	usage: 'report <user>',
+	permission: ADMINISTRATOR | OPERATOR | PREMIUM | STANDARD,
+	async execute(teambot, message) {
+
+		const reported = message.mentions.users.first();
+
+		console.log(reported);
+
+		try {
+			await teambot.db.reports.create({
+				guild: message.guild.id,
+				reporter: message.author.id,
+				reported: reported.id,
+			});
+			return message.reply(`${reported} has been reported.`);
+		}
+		catch (e) {
+			console.log(e);
+			return message.reply('Something went wrong creating a report.');
+		}
+	},
+};
