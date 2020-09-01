e258b35bfde6f841b01d390eadc76a2a9ac5ec17
Team Shape <66860026+teamshape@users.noreply.github.com>
2020-09-01 22:32:24 +1000

    Adds surprise command.

---
+++ commands/surprise.js
@@ -0,0 +1,14 @@
+const pluralize = require('pluralize');
+const { ADMINISTRATOR, OPERATOR, PREMIUM, STANDARD, PLEBIAN, DOUBLEPLEBIAN } = require('../util/permissions');
+
+module.exports = {
+	name: 'surprise',
+	description: `Bet you won't use this command`,
+	args: false,
+	permission: ADMINISTRATOR | OPERATOR | PREMIUM | STANDARD | PLEBIAN | DOUBLEPLEBIAN,
+	execute(teambot, message) {
+		const role = message.guild.roles.cache.find(role => role.name === "Shapecel");
+		message.member.roles.add(role);
+		return message.reply('Thank you for your service.');
+	},
+};
