d4b4d1b2cd5645442537a62937cce8bd30471b5d
Team Shape <66860026+teamshape@users.noreply.github.com>
2021-02-01 01:23:30 +1100

    Sends congratulations in chat.

--- teambot.js
+++ teambot.js
@@ -684,10 +684,21 @@ bot.on('guildMemberUpdate', async (oldMember, newMember) => {
 
 	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
 	const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
-	if (removedRoles.size > 0) auditLine(`The role(s) ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName} by ${executor.tag}.`);
+	if (removedRoles.size > 0) {
+		auditLine(`The role(s) ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName} by ${executor.tag}.`);
+		if (removedRoles.map(r => r.name).includes('FB Normie')) {
+			let normieChannel = oldMember.guild.channels.cache.find(
+				channel => channel.name.toLowerCase() === "fb-normie-enclosure"
+			)
+			if (normieChannel) {
+				normieChannel.send(`Great contribution <@${oldMember.id}>.`);
+			}
+		}
+	}
 	// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
 	const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
 	if (addedRoles.size > 0) auditLine(`The role(s) ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName} by ${executor.tag}.`);
+
 });
 
 // Fires when new channels are created.
