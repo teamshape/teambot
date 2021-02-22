25a90e03825f24012211c94b245e08924d0d64d5
Team Shape <66860026+teamshape@users.noreply.github.com>
2021-02-23 00:00:23 +1100

    Adds time command.

---
+++ commands/time.js
@@ -0,0 +1,24 @@
+const { ADMINISTRATOR, OPERATOR, PREMIUM, STANDARD } = require('../util/permissions');
+const moment = require('moment-timezone');
+const { BOTCHANNEL } = require('../util/channels');
+
+module.exports = {
+	name: 'time',
+	description: 'Shows time and date information (usually for debugging timezones).',
+	args: false,
+	usage: 'time',
+	permission: ADMINISTRATOR | OPERATOR | PREMIUM | STANDARD,
+	channel: BOTCHANNEL,
+	async execute(teambot, message, args) {
+
+		const local = moment().format();
+		const aus = moment().tz('Australia/Sydney').format();
+
+		const data = [];
+		data.push("Let's look up some times!");
+		data.push(`Server time: ${local}.`);
+		data.push(`Australia time: ${aus}.`);
+
+		return message.reply(data, { split: true });
+	},
+};
