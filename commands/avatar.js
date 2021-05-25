355d26b3c53760d2b93eb38c871ccd782c466d1f
Team Shape <66860026+teamshape@users.noreply.github.com>
2021-05-25 23:30:29 +1000

    Adds avatar command.

---
+++ commands/avatar.js
@@ -0,0 +1,21 @@
+const { BOTCHANNEL } = require('../util/channels');
+const { ADMINISTRATOR, OPERATOR, PREMIUM, STANDARD } = require('../util/permissions');
+
+module.exports = {
+	name: 'avatar',
+	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
+	aliases: ['icon', 'pfp', 'a'],
+	permission: ADMINISTRATOR | OPERATOR | PREMIUM | STANDARD,
+	channel: BOTCHANNEL,
+	async execute(teambot, message, args) {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`${message.author.displayAvatarURL({ dynamic: true, format: 'png' })}`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.displayAvatarURL({ dynamic: true })}`;
+		});
+
+		message.channel.send(avatarList);
+	},
+};
