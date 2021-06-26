efd03dbe1e7acb53ccea517e1ee4a99370558fe4
Team Shape <66860026+teamshape@users.noreply.github.com>
2021-06-27 09:23:55 +1000

    Changes help text to be more accurate.

--- commands/botmanage.js
+++ commands/botmanage.js
@@ -45,7 +45,7 @@ module.exports = {
 			return message.author.send(data, { split: true })
 				.then(() => {
 					if (message.channel.type === 'dm') return;
-					message.reply('I\'ve sent you a DM with all my commands!');
+					message.reply('I\'ve sent you a DM with all my welcomes!');
 				})
 				.catch(error => {
 					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
@@ -96,7 +96,7 @@ module.exports = {
 			return message.author.send(data, { split: true })
 				.then(() => {
 					if (message.channel.type === 'dm') return;
-					message.reply('I\'ve sent you a DM with all my commands!');
+					message.reply('I\'ve sent you a DM with all my botlines!');
 				})
 				.catch(error => {
 					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
